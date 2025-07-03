371eabe9059d89a81c47cacb602e6574f4d985be
Chirag sood <chiragsood25@gmail.com>
2025-07-03 11:10:24 +0530

    chore: create Record class skeleton with fields

--- src/com/chirag/financetracker/Transaction.java
+++ src/com/chirag/financetracker/Transaction.java
@@ -1,4 +1,12 @@
 package com.chirag.financetracker;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Transaction {
-}
+        private LocalDate   date;
+        private String      type;      // "INCOME" or "EXPENSE"
+        private String      category;
+        private String      notes;
+        private BigDecimal  amount;
+    }
+
