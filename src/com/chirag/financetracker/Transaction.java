cbadef741ea7973d8f6c11271092fcf3c860ae6c
Chirag sood <chiragsood25@gmail.com>
2025-07-03 11:24:44 +0530

    feat: add constructor to initialize Record fields

--- src/com/chirag/financetracker/Transaction.java
+++ src/com/chirag/financetracker/Transaction.java
@@ -1,12 +1,23 @@
 package com.chirag.financetracker;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Transaction {
-        private LocalDate   date;
-        private String      type;      // "INCOME" or "EXPENSE"
-        private String      category;
-        private String      notes;
-        private BigDecimal  amount;
+    private LocalDate date;
+    private String type;      // "INCOME" or "EXPENSE"
+    private String category;
+    private String notes;
+    private BigDecimal amount;
+
+    public Transaction(LocalDate date, String type, String category, String description, BigDecimal amount) {
+        this.date = date;               // Set the date.
+        this.type = type;               // Set the type.
+        this.category = category;       // Set the category.
+        this.notes = notes;             // Extra details
+        this.amount = amount;           // Set the amount.
     }
+}
+
+
 
